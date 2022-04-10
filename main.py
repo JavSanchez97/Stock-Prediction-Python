4b17f1d39b0fdaa2c97aa327b2956adafdef89c2
JavSanchez97 <javsanchez2015@gmail.com>
2022-04-10 19:19:08 -0400

    Final file

---
+++ main.py
@@ -0,0 +1,61 @@
+#import all neccesary libraries
+import streamlit as st
+from datetime import date
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title("Stock Prediction App")
+
+stocks = ("AAPL", "MSFT", "VTI", "VOO", "SONY", "PTON")
+selected_stock = st.selectbox("Select dataset for prediction", stocks)
+
+n_years = st.slider("Years of prediction:", 1,3)
+period = n_years * 365
+
+#st.cache will cache the data so it does not have to run this code again for a stock that has been selected
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text("Load Data...")
+data = load_data(selected_stock)
+data_load_state.text("Loading data...Done!")
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
+
+# Forecasting
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"}) #must change column names to match FB Prophet specifications
+
+m = Prophet()
+m.fit(df_train) #fits training data and begins the training
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future) #this will return the prediciton to the forecast data frame
+
+st.subheader('Forecast data')
+st.write(forecast.tail())
+
+st.write('forecast data')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write('forecast components')
+fig2 = m.plot_components(forecast)
+st.write(fig2)
